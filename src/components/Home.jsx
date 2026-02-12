0ff046c39389923357926754c73423fcc23d9a5a
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2026-02-12 13:30:08 -0800

    working on home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='bg-[#1C1C1C] w-screen h-screen'>
-        <div></div>
+       <div className='text-white pt-[18vh] flex flex-col justify-center items-center '>
+        <div><img className='w-[40vw]' src="/images/logo.png" alt="Logo" /></div>
+        <div className='fonttwo -mt-1 pl-[4vw] tracking-[3.5em] text-[1.3vw] font-semibold text-center'>STUDIO</div>
+        <div className='fontone mt-4 tracking-normal font-light text-[1.2vw]'>Interior design studio based in London</div>
+       </div>
     </div>
   )
 }
