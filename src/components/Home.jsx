bf6ed77dddc49ae2e3c3bd4428c58d260e0e92b0
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2026-03-19 21:55:55 -0700

    res

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 
 const Home = () => {
-    const boxes = (
+const boxes = (
   <> 
-    <div className='h-[40vh] w-[15vw] mt-[5vh] ml-5 rounded-tr-[3vw] rounded-xl 
-    bg-[url("/images/marquee/mone.png")] bg-no-repeat bg-cover '></div>
-    <div className='h-[40vh] w-[15vw] mt-[5vh] rounded-bl-[3vw] rounded-xl 
-    bg-[url("/images/marquee/mtwo.png")] bg-no-repeat bg-cover'></div>
-    <div className='h-[40vh] w-[15vw] mt-[5vh] rounded-tr-[3vw] rounded-xl 
-    bg-[url("/images/marquee/mthree.png")] bg-no-repeat bg-cover'></div>
-    <div className='h-[40vh] w-[15vw] mt-[5vh] rounded-bl-[3vw] rounded-xl 
-    bg-[url("/images/marquee/mfour.png")] bg-no-repeat bg-cover'></div>
-    <div className='h-[40vh] w-[15vw] mt-[5vh] rounded-tr-[3vw] rounded-xl 
-    bg-[url("/images/marquee/mfive.png")] bg-no-repeat bg-cover'></div>
-    <div className='h-[40vh] w-[15vw] mt-[5vh] rounded-bl-[3vw] rounded-xl 
-    bg-[url("/images/marquee/msix.png")] bg-no-repeat bg-cover'></div>
+    <div className='h-[28vh] lg:h-[40vh] w-[45vw] md:w-[22vw] lg:w-[15vw] mt-[5vh] ml-5 rounded-tr-[3vw] rounded-xl bg-[url("/images/marquee/mone.png")] bg-no-repeat bg-cover'></div>
+
+    <div className='h-[28vh] lg:h-[40vh] w-[45vw] md:w-[22vw] lg:w-[15vw] mt-[5vh] rounded-bl-[3vw] rounded-xl bg-[url("/images/marquee/mtwo.png")] bg-no-repeat bg-cover'></div>
+
+    <div className='h-[28vh] lg:h-[40vh] w-[45vw] md:w-[22vw] lg:w-[15vw] mt-[5vh] rounded-tr-[3vw] rounded-xl bg-[url("/images/marquee/mthree.png")] bg-no-repeat bg-cover'></div>
+
+    <div className='h-[28vh] lg:h-[40vh] w-[45vw] md:w-[22vw] lg:w-[15vw] mt-[5vh] rounded-bl-[3vw] rounded-xl bg-[url("/images/marquee/mfour.png")] bg-no-repeat bg-cover'></div>
+
+    <div className='h-[28vh] lg:h-[40vh] w-[45vw] md:w-[22vw] lg:w-[15vw] mt-[5vh] rounded-tr-[3vw] rounded-xl bg-[url("/images/marquee/mfive.png")] bg-no-repeat bg-cover'></div>
+
+    <div className='h-[28vh] lg:h-[40vh] w-[45vw] md:w-[22vw] lg:w-[15vw] mt-[5vh] rounded-bl-[3vw] rounded-xl bg-[url("/images/marquee/msix.png")] bg-no-repeat bg-cover'></div>
   </>
 );
-
   return (
-    <div className='bg-[#1C1C1C] w-screen h-screen'>
+    <div className='bg-[#1C1C1C] w-screen h-full lg:h-screen'>
        <div className='text-white pt-[18vh] flex flex-col justify-center items-center '>
-        <div><img className='w-[40vw]' src="/images/logo.png" alt="Logo" /></div>
-        <div className='fonttwo -mt-1 pl-[4vw] tracking-[3.5em] text-[1.3vw] font-semibold text-center'>STUDIO</div>
-        <div className='fontone mt-4 tracking-normal font-light text-[1.2vw]'>Interior design studio based in London</div>
+        <div><img className='w-[80vw] md:w-[60vw] lg:w-[40vw]' src="/images/logo.png" alt="Logo" /></div>
+        <div className='fonttwo -mt-1 pl-[10vw] md:pl-[6vw] lg:pl-[4vw] tracking-[3.5em] text-[3vw] md:text-[2vw] lg:text-[1.3vw] font-semibold text-center'>STUDIO</div>
+        <div className='fontone mt-4 tracking-normal font-light text-[4vw] md:text-[3vw] lg:text-[1.2vw]'>Interior design studio based in London</div>
       <div className="overflow-hidden w-full">
   <div className="marquee">
     <div className="flex gap-5">
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Text = () => {
   return (
-    <div className='bg-[#1C1C1C] px-[7.5vw] w-screen flex justify-center items-center h-[65vh]'>
-        <div className='fontone text-white text-[2.2vw] text-center font-semibold'>"At VOID STUDIO, we approach interior design with clarity and intention, shaping spaces through balance, proportion, and thoughtful restraint. By stripping away the unnecessary, we allow materials, light, and structure to define the experience, resulting in interiors that feel calm, purposeful, and enduring. Every project is a careful composition that supports how a space is lived in creating refined environments that remain both functional and relevant over time."</div>
+    <div className='bg-[#1C1C1C] px-[7.5vw] lg:py-0 py-10 w-screen flex justify-center items-center h-full lg:h-[65vh]'>
+        <div className='fontone  text-white text-[3.5vw]  md:text-[2.2vw] text-center md:font-semibold'>"At VOID STUDIO, we approach interior design with clarity and intention, shaping spaces through balance, proportion, and thoughtful restraint. By stripping away the unnecessary, we allow materials, light, and structure to define the experience, resulting in interiors that feel calm, purposeful, and enduring. Every project is a careful composition that supports how a space is lived in creating refined environments that remain both functional and relevant over time."</div>
     </div>
   )
 }
